c1079d179561b2f96c50ff5bf02e3f51c65c5d36
martin-centurion <martinalejandrocenturion@gmail.com>
2023-03-06 23:32:41 -0300

    limpiando archivos

--- src/App.js
+++ src/App.js
@@ -2,18 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>App</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>App</h1>
     </div>
   );
 }
